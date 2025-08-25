5f93ce1d65b50e513a7b5d812bde85b2d3e37bc2
Nikita <fedorow.nickit@gmail.com>
2025-08-26 02:13:02 +0300

    test: Configure test environment with isolated database setup

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,39 +2,39 @@ import asyncio
 from asyncio import AbstractEventLoop
 from collections.abc import AsyncGenerator
 
-import motor.motor_asyncio
 import pytest
 from httpx import ASGITransport, AsyncClient
 
-from core import settings
-from core.database import initialize_database
+
 from src.app.app import app
+from src.core import settings
+from src.core.database.client import get_mongo_client
+from src.core.database.registry import initialize_database
 
 
 @pytest.fixture(scope="session")
 def event_loop() -> AbstractEventLoop:
-    """Ивент луп"""
     return asyncio.get_event_loop()
 
 
-@pytest.fixture(autouse=True, scope="session")
-async def init_db() -> None:
-    """Ининциализировать базу данных"""
+@pytest.fixture(autouse=True, scope="function")
+async def setup_database() -> None:
+    """
+    Подготавливает чистую и инициализированную БД перед каждым тестом.
+    """
+    if not settings.mongo.db_name.lower().endswith("test"):
+        raise RuntimeError(
+            f"Aborting tests: DB name '{settings.mongo.db_name}' does not end with 'test'."
+        )
+
+    client = get_mongo_client()
+    await client.drop_database(settings.mongo.db_name)
+
     await initialize_database()
 
 
-@pytest.fixture(autouse=True)
-async def drop_db() -> None:
-    """Дропнуть бд перед каждым тестом"""
-    if not settings.mongo.db_name.lower().endswith("test"):
-        raise RuntimeError
-
-    mongo: motor.motor_asyncio.AsyncIOMotorClient = motor.motor_asyncio.AsyncIOMotorClient(settings.mongo.url)
-    await mongo.drop_database(settings.mongo.db_name)
-
-
 @pytest.fixture(scope="session")
-async def client() -> AsyncGenerator[AsyncClient]:
+async def client() -> AsyncGenerator[AsyncClient, None]:
     """Получить тестовый клиент"""
     async with AsyncClient(
         transport=ASGITransport(app=app),
